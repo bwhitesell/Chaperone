f67d2e2591184689541cdf94ecf78bd7034115e8
Ben Whitesell <whitesell.ben@gmail.com>
2019-04-02 20:12:26 -0700

    Started work on homepage

--- crymeweb/crymeweb/urls.py
+++ crymeweb/crymeweb/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
+from django.views.generic import TemplateView
 from django.urls import path
 
 from safety.views import SafetyAnalysisView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('safety-analysis/poc', SafetyAnalysisView.as_view())
+    path('api/safety-analysis', SafetyAnalysisView.as_view()),
+    path('', TemplateView.as_view(template_name='home.html'),)
 ]

dbb529afa7f251058d3f00872a5972ab805f0d90
Ben Whitesell <whitesell.ben@gmail.com>
2019-04-09 23:06:39 -0700

    expanding view functionality

--- crymeweb/safety/views.py
+++ crymeweb/safety/views.py
@@ -1,5 +1,6 @@
+from django.http import HttpResponse, HttpResponseBadRequest
+from django.shortcuts import redirect
 from django.views import View
-from django.http import HttpResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -29,12 +30,29 @@ class SafetyAnalysisAPIView(APIView):
 
 
 class SafetyAnalysisView(View):
+    def get(self, request):
+        lat = request.GET.get('lat', None)
+        lon = request.GET.get('lon', None)
+        if lat and lon:
+            try:
+                lat = float(lat)
+                lon = float(lon)
+            except ValueError:
+                HttpResponseBadRequest('Invalid query string.')
+            if GeometricRegion.objects.in_domain(float(lon), float(lat)):
+                return HttpResponse('hey')
+            else:
+                return HttpResponseBadRequest('lat/lon outside city limits of los angeles.')
+
+        return HttpResponseBadRequest('Please provide lat/lon query parameters.')
+
+
     def post(self, request):
-        form = LatLonForm(request.POST)  # A form bound to the POST data
+        form = LatLonForm(request.POST)
         print(form)
         print(form.data)
         if form.is_valid():
-            print(form.cleaned_data['latitude'])
-            print(form.cleaned_data['longitude'])
-
-        return HttpResponse('hello world')
+            lat = form.cleaned_data['latitude']
+            lon = form.cleaned_data['longitude']
+            return redirect('/safety-analysis?lat=' + str(lat) + '&lon=' + str(lon))
+        return HttpResponseBadRequest('Please provide lat/lon parameters.')

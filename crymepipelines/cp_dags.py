fc9cb891ca8cec66a17903e015a358e4c33658f1
Ben Whitesell <whitesell.ben@gmail.com>
2019-05-07 08:38:50 -0700

    configured airflow successfully

--- crymepipelines/cp_dags.py
+++ crymepipelines/cp_dags.py
@@ -5,6 +5,7 @@ from datetime import datetime, timedelta
 
 default_args = {
     'owner': 'airflow',
+    'start_date': datetime(2019, 5, 1),
     'depends_on_past': False,
     'email': ['airflow@example.com'],
     'email_on_failure': False,
@@ -13,34 +14,38 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
-dag = DAG('CrymeFeeder Train Model', default_args=default_args, schedule_interval=timedelta(days=1))
+base_spark_submit = 'cd $HOME/.envs/cc/CrymeClarity/crymepipelines/dist && spark-submit --py-files shared.zip,tasks.zip '
+
+dag = DAG('train_cryme_classifier', default_args=default_args, schedule_interval='*/5 * * * *', catchup=False)
 
 # t1, t2 and t3 are examples of tasks created by instantiating operators
 
-base_spark_submit = 'cd $HOME/.envs/cc/CrymeClarity/crymepipelines/dist && spark-submit --py-files shared.zip,tasks.zip'
 t1 = BashOperator(
-    task_id='',
+    task_id='generate_location_time_samples',
     bash_command=base_spark_submit + 'run.py --task GenerateLocationTimeSamples',
     dag=dag)
 
 t2 = BashOperator(
-    task_id='',
+    task_id='build_dataset',
     bash_command=base_spark_submit + 'run.py --task BuildDataset',
     dag=dag)
 
 t3 = BashOperator(
-    task_id='',
+    task_id='clean_dataset',
     bash_command=base_spark_submit + 'run.py --task CleanDataset',
     dag=dag)
 
 t4 = BashOperator(
-    task_id='',
+    task_id='engineer_features',
     bash_command=base_spark_submit + 'run.py --task EngineerFeatures',
     dag=dag)
 
 t5 = BashOperator(
-    task_id='',
-    bash_command=base_spark_submit + 'run.py --task EngineerFeatures',
+    task_id='train_model',
+    bash_command=base_spark_submit + 'run.py --task TrainCrymeClassifier',
     dag=dag)
 
 t2.set_upstream(t1)
+t3.set_upstream(t2)
+t4.set_upstream(t3)
+t5.set_upstream(t4)

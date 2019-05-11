7494835bc29491b8c312c1554a1c9e2e5d264468
Ben Whitesell <whitesell.ben@gmail.com>
2019-05-11 12:26:40 -0700

    Added dashboard bootstrap-template

--- crymeweb/crymeweb/urls.py
+++ crymeweb/crymeweb/urls.py
@@ -1,13 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 
-from safety.views import SafetyAnalysisView, SafetyAnalysisAPIView, home_view, TodaysSafetyAnalysisAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/safety-analysis', SafetyAnalysisAPIView.as_view()),
-    path('api/safety-analysis-today', TodaysSafetyAnalysisAPIView.as_view(), name='day_safety_analysis'),
-    path('', home_view),
-    path('safety-analysis', SafetyAnalysisView.as_view())
 ]
 

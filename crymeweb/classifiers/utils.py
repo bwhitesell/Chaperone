902d18d9e7abe83b1406db3261df150453f53079
Ben Whitesell <whitesell.ben@gmail.com>
2019-05-27 10:54:25 -0700

    Added homepage to crymeweb

--- crymeweb/classifiers/utils.py
+++ crymeweb/classifiers/utils.py
@@ -1,6 +1,10 @@
 # utils for the classifiers app
 
 
+lat_delta = 0.004363475000000037/2
+lon_delta = 0.007254180000003885/2
+
+
 def datetime_to_minutes_into_day(ts):
     hours = ts.hour
     minutes = ts.minute
@@ -11,8 +15,9 @@ def build_view_box(lon, lat):
     return {
         'point_lat': lat,
         'point_lon': lon,
-        'bl_lat': lat + 0.004363475000000037,
-        'bl_lon': lon - 0.007254180000003885,
-        'tr_lat': lat - 0.004363475000000037,
-        'tr_lon': lon + 0.007254180000003885,
+        'bl_lat': lat + lat_delta,
+        'bl_lon': lon - lon_delta,
+        'tr_lat': lat - lat_delta,
+        'tr_lon': lon + lon_delta,
     }
+
--- crymeweb/classifiers/views.py
+++ crymeweb/classifiers/views.py
@@ -2,16 +2,17 @@ import numpy as np
 
 from django.shortcuts import render
 
+from crime.models import CrimeIncident
 from .models import CrymeClassifier
-from .utils import build_view_box
+from .utils import build_view_box, lat_delta, lon_delta
 
 
 models = CrymeClassifier.objects.load_models()
 
 
 def dashboard_view(request):
-    longitude = 34.066426
-    latitude = -118.471124
+    longitude = 34.101574
+    latitude = -118.334366
 
     feature_vectors = np.array([[longitude, latitude, time] for time in range(0, 1440, 60)])
 
@@ -21,10 +22,19 @@ def dashboard_view(request):
         percentage_scaled_positive_prediction_vector = models[model].predict_proba(feature_vectors)[:, 1] * 100
         charting_data[model.target] = {
             'values': percentage_scaled_positive_prediction_vector.tolist(),
-            'max': round(percentage_scaled_positive_prediction_vector.max(), 4) * 1.1
+            'max': round(percentage_scaled_positive_prediction_vector.max() * 1.1, 2)
         }
 
     charting_data['max'] = max([charting_data[model_target]['max'] for model_target in charting_data])
+
+    # get recent crimes nearby
+    nearby_crimes = CrimeIncident.objects.filter(
+        lat__lte=latitude + lat_delta,
+        lat__gte=latitude - lat_delta,
+        lon__lte=longitude + lat_delta,
+        lon__gte=longitude - lat_delta,
+    )
+
 
     # build real-time risk estimates
     real_time_risk_ests = []
@@ -41,8 +51,9 @@ def dashboard_view(request):
         'charting_data': charting_data,
         'risk_ests_row_1': real_time_risk_ests[:4],
         'risk_ests_row_2': real_time_risk_ests[4:8],
-        'view_box': build_view_box(longitude, latitude)
+        'view_box': build_view_box(longitude, latitude),
+        'nearby_crimes': nearby_crimes,
     }
     print(real_time_risk_ests)
-    return render(request, 'dashboard.html', context)
+    return render(request, 'home.html', context)
 
